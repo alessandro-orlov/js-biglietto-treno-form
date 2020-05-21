aa88bc227c340f36a3a7d391a92c5dac4e93e137
Alessandro <alex.ixs@hotmail.com>
2020-05-21 19:55:08 +0200

    almost done

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,23 @@
 // Passenger FORM data
 var formName = document.getElementById('form-name');
 var distanceByTrain = document.getElementById('distance-by-train');
+var ageDiscount = document.getElementById('age-discount');
 
+//Undefind (for now) variables
+var ageDiscountValue;
 
 //Ticket fields
 var ticketName = document.getElementById('ticket-name');
 var ticketPrice = document.getElementById('ticket-price');
+var carriage = document.getElementById('carriage');
+var codeCp = document.getElementById('code-cp');
 
+var ticketPrice = distanceByTrain.value * 0.21;
+console.log(ticketPrice);
 
 //button
 var generateTicketButton = document.getElementById('generate-ticket-button');
 var resetFormButton = document.getElementById('reset-form');
-
 
 //Event that generate ticket output
 generateTicketButton.addEventListener('click',
@@ -19,8 +25,17 @@ generateTicketButton.addEventListener('click',
     // Passenger Name
     ticketName.innerHTML = formName.value;
 
-    //Price based on km
-    ticketPrice.innerHTML = (distanceByTrain.value * 0.21).toFixed(2) + '€';
+    //generate random carriage number
+    carriage.innerHTML = Math.floor(Math.random() * 46);
+
+    //generate random code CP
+    codeCp.innerHTML = Math.floor(Math.random() * 100001);
+
+    //Price
+    // ticketPrice.innerHTML = (distanceByTrain.value * 0.21).toFixed(2);
+    // if( ageDiscount.value == 'junior' ) {
+    //   ticketPrice.innerHTML = ticketPrice - (distanceByTrain.value * 0.21).toFixed(2);
+    // }
   }
 );
 
@@ -28,11 +43,18 @@ generateTicketButton.addEventListener('click',
 resetFormButton.addEventListener('click',
   function() {
     //reset fields (Passenger Name) for form and ticket
-    formName.value = "&nbsp;";
-    ticketName.innerHTML = "&nbsp;";
+    formName.value = '';
+    ticketName.innerHTML = '&nbsp;';
 
     //reset fields distanceByTrain in the form & price in the ticket
-    distanceByTrain.value = "&nbsp;";
+    distanceByTrain.value = '&nbsp;';
     ticketPrice.innerHTML = '&nbsp;';
+
+    //Code & code-cp reset
+    carriage.innerHTML = '&nbsp;';
+    codeCp.innerHTML = '&nbsp;';
+
+    //reset ageDiscount field
+    ageDiscount.value = 'standard';
   }
 )
