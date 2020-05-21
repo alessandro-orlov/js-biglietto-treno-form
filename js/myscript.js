4528f812847cb64926d90fa10b526173761bd3e4
Alessandro <alex.ixs@hotmail.com>
2020-05-21 17:43:43 +0200

    added some js

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,24 @@
+// Passenger form data
+var formName = document.getElementById('form-name');
+console.log(formName.value)
+var generateTicket = document.getElementById('generate-ticket"')
 
+
+
+
+//Ticket output
+var ticketName = document.getElementById('ticket-name');
+
+
+//Event that generate ticket
+generateTicket.addEventListener('click',
+  function(){
+    ticketName.innerHTML = formName.value;
+  }
+);
+
+//
+// addEventListener("click",
+// function(){
+//   document.getElementById("demo").innerHTML = "Hello World";
+// });
