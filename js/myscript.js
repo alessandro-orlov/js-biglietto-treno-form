064fc5d76ecfdb3899dbec2d4cfed7988f70f735
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-05-21 22:43:12 +0200

    Complete

--- js/myscript.js
+++ js/myscript.js
@@ -2,40 +2,44 @@
 var formName = document.getElementById('form-name');
 var distanceByTrain = document.getElementById('distance-by-train');
 var ageDiscount = document.getElementById('age-discount');
-
-//Undefind (for now) variables
-var ageDiscountValue;
-
-//Ticket fields
-var ticketName = document.getElementById('ticket-name');
-var ticketPrice = document.getElementById('ticket-price');
-var carriage = document.getElementById('carriage');
-var codeCp = document.getElementById('code-cp');
-
-var ticketPrice = distanceByTrain.value * 0.21;
-console.log(ticketPrice);
-
-//button
+//Evet buttons
 var generateTicketButton = document.getElementById('generate-ticket-button');
 var resetFormButton = document.getElementById('reset-form');
 
+//Ticket fields
+var ticketName = document.getElementById('ticket-name');
+var showDiscound = document.getElementById('discount');
+var carriage = document.getElementById('carriage');
+var codeCp = document.getElementById('code-cp');
+var finalPrice = document.getElementById('ticket-price');
+
+
 //Event that generate ticket output
 generateTicketButton.addEventListener('click',
-  function(){
+  function() {
     // Passenger Name
     ticketName.innerHTML = formName.value;
+
+    //Show applied discount
+    showDiscound.innerHTML = ageDiscount.value
 
     //generate random carriage number
     carriage.innerHTML = Math.floor(Math.random() * 46);
 
-    //generate random code CP
+    //generate random codeCP
     codeCp.innerHTML = Math.floor(Math.random() * 100001);
 
-    //Price
-    // ticketPrice.innerHTML = (distanceByTrain.value * 0.21).toFixed(2);
-    // if( ageDiscount.value == 'junior' ) {
-    //   ticketPrice.innerHTML = ticketPrice - (distanceByTrain.value * 0.21).toFixed(2);
-    // }
+    //Ticket price calculations
+    var ticketPrice = distanceByTrain.value * .21;
+    console.log(ticketPrice)
+    if(ageDiscount.value == 'junior') {
+      ticketPrice = ticketPrice - (ticketPrice * 0.2);
+    } else if (ageDiscount.value == 'senior') {
+      ticketPrice = ticketPrice - (ticketPrice * 0.4);
+    }
+
+    //Show final price
+    finalPrice.innerHTML = ticketPrice + '€';
   }
 );
 
@@ -44,15 +48,18 @@ resetFormButton.addEventListener('click',
   function() {
     //reset fields (Passenger Name) for form and ticket
     formName.value = '';
-    ticketName.innerHTML = '&nbsp;';
+    ticketName.innerHTML = '';
+
+    //reset showDiscound
+    showDiscound.innerHTML = '';
 
     //reset fields distanceByTrain in the form & price in the ticket
-    distanceByTrain.value = '&nbsp;';
-    ticketPrice.innerHTML = '&nbsp;';
+    distanceByTrain.value = '';
+    finalPrice.innerHTML = '';
 
     //Code & code-cp reset
-    carriage.innerHTML = '&nbsp;';
-    codeCp.innerHTML = '&nbsp;';
+    carriage.innerHTML = '';
+    codeCp.innerHTML = '';
 
     //reset ageDiscount field
     ageDiscount.value = 'standard';
