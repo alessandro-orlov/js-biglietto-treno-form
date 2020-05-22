f4763befbcccd415b47a24e83cae9aa7bfd0dc61
Alessandro <alex.ixs@hotmail.com>
2020-05-22 14:51:04 +0200

    Error message logic complete

--- js/myscript.js
+++ js/myscript.js
@@ -1,85 +1,91 @@
-// Passenger FORM data
-var formName = document.getElementById('form-name');
-var distanceByTrain = document.getElementById('distance-by-train');
-var ageDiscount = document.getElementById('age-discount');
 //Evet buttons
 var generateTicketButton = document.getElementById('generate-ticket-button');
 var resetFormButton = document.getElementById('reset-form');
 
+// Passenger FORM data
+var formName = document.getElementById('form-name');
+var distanceByTrain = document.getElementById('distance-by-train');
+var ageDiscount = document.getElementById('age-discount');
+var error = document.getElementById('error-msg')
+
 //Ticket fields
+var showTheTicket = document.getElementById('ticket-output')
 var ticketName = document.getElementById('ticket-name');
 var showDiscount = document.getElementById('discount');
 var carriage = document.getElementById('carriage');
 var codeCp = document.getElementById('code-cp');
 var finalPrice = document.getElementById('ticket-price');
-var showTheTicket = document.getElementById('ticket-output')
+
 
 //Event that generate ticket output
 generateTicketButton.addEventListener('click',
   function() {
-    //Error empty field
+    //Error message
     if((formName.value == '') || (distanceByTrain.value == '')) {
-      alert('Errore: Non hai compilato tutti i campi!')
+      error.className = 'visible';
+      showTheTicket.className = 'hidden';
+    } else {
+
+      //Hide error message
+      error.className = 'hidden';
+
+      //Show the ticket
+      showTheTicket.className = 'visible';
+
+      //Ticket output
+      ticketName.innerHTML = formName.value;
+      carriage.innerHTML = Math.floor(Math.random() * 41);
+      codeCp.innerHTML = Math.floor(Math.random() * 100001);
+
+      //Ticket price calculations & Discount message
+      var ticketPrice = (distanceByTrain.value * .21).toFixed(2);
+
+      if(ageDiscount.value == 'junior') {
+        ticketPrice = (ticketPrice - (ticketPrice * 0.2)).toFixed(2);
+        showDiscount.innerHTML = 'Little Noob';
+      } else if (ageDiscount.value == 'senior') {
+        ticketPrice = (ticketPrice - (ticketPrice * 0.4)).toFixed(2);
+        showDiscount.innerHTML = 'Old fart';
+      } else if(ageDiscount.value == 'standard') {
+        showDiscount.innerHTML = "Just pay"
+      }
+
+      //Special price for my mentors
+      if((formName.value == 'Sainato') || (formName.value == 'sainato') || (formName.value == 'Scolozzi') || (formName.value == 'scolozzi') || (formName.value == 'Mosca') || (formName.value == 'Mosca')) {
+        showDiscount.innerHTML = 'Gratis';
+        ticketPrice = 'Gratis';
+      }
+
+      //Show final price
+      finalPrice.innerHTML = ticketPrice + '€';
+      if(ticketPrice == 'Gratis' ) {
+        finalPrice.innerHTML = 'GRATIS';
+      }
+
     }
-
-    //Show the ticket
-    showTheTicket.className = 'visible';
-
-    // Passenger Name
-    ticketName.innerHTML = formName.value;
-
-    //Generate random carriage number
-    carriage.innerHTML = Math.floor(Math.random() * 41);
-
-    //Generate random codeCP
-    codeCp.innerHTML = Math.floor(Math.random() * 100001);
-
-    //Ticket price calculations & Discount message
-    var ticketPrice = (distanceByTrain.value * .21).toFixed(2);
-    if(ageDiscount.value == 'junior') {
-      ticketPrice = (ticketPrice - (ticketPrice * 0.2)).toFixed(2);
-      showDiscount.innerHTML = 'Little Noob';
-    } else if (ageDiscount.value == 'senior') {
-      ticketPrice = (ticketPrice - (ticketPrice * 0.4)).toFixed(2);
-      showDiscount.innerHTML = 'Old fart';
-    } else if(ageDiscount.value == 'standard') {
-      showDiscount.innerHTML = "Just pay"
-    }
-
-    //Special price for my mentors
-    if((formName.value == 'Sainato') || (formName.value == 'sainato') || (formName.value == 'Scolozzi') || (formName.value == 'scolozzi') || (formName.value == 'Mosca') || (formName.value == 'Mosca')) {
-      showDiscount.innerHTML = 'Gratis';
-      ticketPrice = 'gratis';
-    }
-
-
-    //Show final price
-    finalPrice.innerHTML = ticketPrice + '€';
   }
 );
 
-//Event that reset Form
+//Event that reset Form & hide ticket
 resetFormButton.addEventListener('click',
   function() {
-    //Reset fields (Passenger Name) for form and ticket
-    formName.value = '';
-    ticketName.innerHTML = '';
-
     //Hide the ticket
     showTheTicket.className = 'hidden';
 
-    //Reset showDiscount
-    showDiscount.innerHTML = '';
+    //ERROR RESET
+    error.className = 'hidden';
 
-    //Reset fields distanceByTrain in the form & price in the ticket
+    //Passenger Form Reset
+    formName.value = '';
     distanceByTrain.value = '';
-    finalPrice.innerHTML = '';
+    ageDiscount.value = 'standard';
 
-    //Code & code-cp reset
+    //Ticket reset
+    ticketName.innerHTML = '';
+    showDiscount.innerHTML = '';
     carriage.innerHTML = '';
     codeCp.innerHTML = '';
+    finalPrice.innerHTML = '';
 
-    //Reset ageDiscount field
-    ageDiscount.value = 'standard';
   }
 );
